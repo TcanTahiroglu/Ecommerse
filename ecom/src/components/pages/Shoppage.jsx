2dd06269b3434c4aedf6045bdd766305f00b301b
TcanTahiroglu <cantahiroglu73@gmail.com>
2025-02-26 17:19:36 +0300

    Update Shoppage.jsx

--- ecom/src/components/pages/Shoppage.jsx
+++ ecom/src/components/pages/Shoppage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Header from "../layout/Header";
 import Shop from "../layout/shop";
-import Footer from "../layout/footer";
 import Category from "../layout/category";
 
 
